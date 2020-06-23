67b4c3904c87be1af1b7e34f48c55caa59bf272a
Enis Necipoğlu <iletisim@enisnecipoglu.com>
2020-06-24 00:28:29 +0300

    🧪 Add Test for StateManagerBase

---
+++ tests/Xamarin.Forms.StateManager.Tests/Controls/StateManagerBaseTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.StateManager.Controls;
+using Xunit;
+
+namespace Xamarin.Forms.StateManager.Tests.Controls
+{
+    public class StateManagerBaseTests
+    {
+        public StateManagerBaseTests()
+        {
+            
+        }
+
+        [Fact]
+        public void UpdateView_ShouldGenerateExpectedControl_WithExpectedState()
+        {
+            // Arrange
+            var stateName = "State1";
+            var expectedText = "Hello World!";
+            var stateUnderTest = new StateManagerBase<string>();
+            stateUnderTest.States.Add(new StateTemplateBase<string>() 
+            {
+                State = stateName,
+                DataTemplate = new DataTemplate(() => new Label { Text = expectedText }) 
+            });
+
+            // Act
+            stateUnderTest.UpdateView(stateName);
+            var result = stateUnderTest.Children.FirstOrDefault();
+
+            // Assert
+            Assert.IsType<Label>(result);
+            Assert.Equal(expectedText, (result as Label).Text);
+        }
+    }
+}
