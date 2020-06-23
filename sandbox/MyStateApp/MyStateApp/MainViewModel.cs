b5022f42b07562b1e0c9e763f685e7b7a92d1f39
Enis Necipoğlu <iletisim@enisnecipoglu.com>
2020-06-23 23:47:54 +0300

    Update sample

--- sandbox/MyStateApp/MyStateApp/MainViewModel.cs
+++ sandbox/MyStateApp/MyStateApp/MainViewModel.cs
@@ -10,25 +10,36 @@ namespace MyStateApp
 {
     public class MainViewModel : BindableObject
     {
-        private string myState = "Open";
+        private string currentState = "Failed";
+        private string content;
+
+        private static readonly Random random = new Random();
         public MainViewModel()
         {
-            this.ChangeStateCommand = new Command(ChangeState);
+            this.LoadCommand = new Command(Load);
         }
 
-        public string MyState { get => myState; set { myState = value; OnPropertyChanged(); } }
+        public string CurrentState { get => currentState; set { currentState = value; OnPropertyChanged(); } }
 
-        public ICommand ChangeStateCommand { get; set; }
+        public string Content { get => content; set { content = value; OnPropertyChanged(); } }
 
-        private void ChangeState()
+        public ICommand LoadCommand { get; set; }
+
+        private async void Load()
         {
-            if (this.MyState == "Open")
+            this.CurrentState = "Loading";
+
+            await Task.Delay(2000);
+
+            var tmp = random.Next(0, 100);
+            if (tmp > 50)
             {
-                this.MyState = "Closed";
+                this.CurrentState = "Failed";
             }
             else
             {
-                this.MyState = "Open";
+                this.CurrentState = "Loaded";
+                this.Content = "This is loaded content.";
             }
         }
     }
